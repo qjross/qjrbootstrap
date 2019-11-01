f7969959e7908b24c4f4a9c2d0495d63ca1ae08f
Quinten Ross <qjross76@gmail.com>
2019-10-31 22:57:53 -0400

    ya

---
+++ script.js
@@ -0,0 +1,15 @@
+var n = 0
+
+$(document).ready(function() {
+
+$("h1").mouseenter(function(){
+   $(this).css("color" , "gold");
+});
+    
+    $("h1").mouseleave(function(){
+   $(this).css("color" , "black");
+});
+
+    
+});    
+ 
